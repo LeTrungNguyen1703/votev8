67784aa19b85186a034c513b52a395be4067b117
Nguyen <nguyenc2300110@student.ctu.edu.vn>
2025-09-29 08:11:24 +0700

    add user, poll, and vote modules with basic services, controllers, and DTOs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { PollModule } from './poll/poll.module';
+import { VoteModule } from './vote/vote.module';
 
 @Module({
-  imports: [],
+  imports: [ PrismaModule, UserModule, PollModule, VoteModule],
 })
 export class AppModule {}
--- src/applicationContext.ts
+++ src/applicationContext.ts
@@ -1,12 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
+import { PollService } from './poll/poll.service';
+import { VoteService } from './vote/vote.service';
+import { PollModule } from './poll/poll.module';
+import { VoteModule } from './vote/vote.module';
 
 // Mounting the application as bare Nest standalone application so that we can use
 // the Nest services inside our Encore endpoints
-const applicationContext: Promise<{ }> =
+const applicationContext: Promise<{
+  userService: UserService,
+  pollService: PollService,
+  voteService: VoteService,
+}> =
   NestFactory.createApplicationContext(AppModule).then((app) => {
     return {
+      userService: app.select(UserModule).get(UserService, { strict: true }),
+      pollService: app.select(PollModule).get(PollService, { strict: true }),
+      voteService: app.select(VoteModule).get(VoteService, { strict: true }),
     };
   });
 
